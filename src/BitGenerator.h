eb397137380414ef62fc533e546b41c3cf948a9a
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2016-01-22 00:47:43 -0300

    Add BitGenerator interface

---
+++ src/BitGenerator.h
@@ -0,0 +1,26 @@
+#ifndef BIT_GENERATOR_H__
+#define BIT_GENERATOR_H__
+
+/**
+ * Interface for boolean generators
+ *
+ */
+class BitGenerator {
+  public:
+    /**
+     * Pure virtual method to return the current output and advance the generator
+     *
+     * @return the generator's output bit
+     */
+    virtual bool next() noexcept = 0;
+
+    /**
+     * Virtual destructor
+     *
+     */
+    virtual ~BitGenerator() = default;
+};
+
+
+#endif  /* BIT_GENERATOR_H__ */
+
