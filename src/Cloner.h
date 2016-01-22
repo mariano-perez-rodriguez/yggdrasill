7e7912c3405b82788a11b2dca594aa45dfc58b16
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2016-01-21 23:55:15 -0300

    Add Cloner trait

---
+++ src/Cloner.h
@@ -0,0 +1,32 @@
+#ifndef CLONER_H__
+#define CLONER_H__
+
+
+/**
+ * Cloner trait
+ *
+ * @param T  Underlying type to clone
+ */
+template <typename T>
+class Cloner {
+  public:
+    /**
+     * Placement-clone the current object
+     *
+     * @param where  Memory address where to clone
+     * @return a pointer to a placement-new-allocated copy of the current object
+     */
+    virtual T *clone(void *where = nullptr);
+
+    /**
+     * Virtual destructor
+     *
+     */
+    virtual ~Cloner() noexcept = default;
+};
+
+
+#include "Cloner.hpp"
+
+#endif /* CLONER_H__ */
+
---
+++ src/Cloner.hpp
@@ -0,0 +1,25 @@
+#ifndef CLONER_HPP__
+#define CLONER_HPP__
+
+#include "Cloner.h"
+
+#include <type_traits>
+#include <new>
+
+/**
+ * Placement-clone the current object
+ *
+ * @param where  Memory address where to clone
+ * @return a pointer to a placement-new-allocated copy of the current object
+ */
+template <typename T>
+T *Cloner<T>::clone(void *where) {
+  // Make sure we can copy-construct objects of type T
+  static_assert(std::is_copy_constructible<T>::value, "Not copy constructible");
+
+  return nullptr == where ? new T(*static_cast<T *>(this)) : new(where) T(*static_cast<T *>(this));
+}
+
+
+#endif /* CLONER_HPP__ */
+
